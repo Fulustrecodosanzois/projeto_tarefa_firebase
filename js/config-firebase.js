bf024765529faa25e2602f9f1d11cc5fb35c807e
Luciano Silva <luciano_jet@hotmail.com>
2023-10-23 20:27:59 -0300

    criado projeto firebase

---
+++ js/config-firebase.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyDq1Xf8oRSyj0V05Qi9qx8uwh1rWy01dxE",
+   authDomain: "projetotarefagertar.firebaseapp.com",
+   projectId: "projetotarefagertar",
+   storageBucket: "projetotarefagertar.appspot.com",
+   messagingSenderId: "287152429287",
+   appId: "1:287152429287:web:fdaf28fb96b61bc0d96a42"
+ };
+
+ // Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
