939744fd5fef30ac11e25f4e03d1b3865b7845e3
Luciano Silva <luciano_jet@hotmail.com>
2023-10-26 20:39:27 -0300

    iniciado atualização

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 import { app, db } from "./config-firebase.js";
-import { doc, setDoc, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js"
+import { doc, setDoc, collection, addDoc, query, where, getDocs, orderBy,deleteDoc } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js"
 
 let nome = document.querySelector("#tarefa")
 let data = document.querySelector("#data")
 let status = document.querySelector("#status")
 let btnTarefa = document.querySelector("#btnTarefa")
 let bloco = document.querySelector("#bloco")
+let formCadastrar = document.querySelector("#formCadastrar")
+let formAtualizar = document.querySelector("#formAtualizar")
+let btnAtualizar = document.querySelector("#btnAtualizar")
+
 
 async function inserirtarefa() {
     try {
@@ -20,36 +24,70 @@ async function inserirtarefa() {
 
         console.error("ocorreu o seguinte erro: " + error)
     }
-
 }
 
-async function consultarTarefa(){
-    const busca = query(collection(db, "tarefa"), orderby("name"));
+async function consultarTarefa() {
+    bloco.innerHTML = "" // limpando o elemento HTML antes de inserir novos registro, para não acumular dados 
+    const busca = query(collection(db, "tarefa"), orderBy("nome"));
 
     const resultado = await getDocs(busca);
-            resultado.forEach((item) => {
-    // item.data() is never underfine for query item snapshots
-    console.log(item.id, " => ", item.data());
+    resultado.forEach((item) => {
+        // item.data() is never underfine for query item snapshots
+        console.log(item.id, " => ", item.data());
 
-    bloco.innerHTML += `
-            <li class="list-group-item d-flex justify-content-between align-items-center mb-2"
+        bloco.innerHTML += `
+            <li class="list-group-item d-flex justify-content-between align-items-center mt-3">
                 <div class="ms-2 me-auto">
-                <strong>Nome: </strong> ${item.data().name} <br>
-                <strong>Nome: </strong> ${item.data().data} <br>
-                <strong>Nome: </strong> ${item.data().status} <br>
+                <strong>Nome: </strong> ${item.data().nome} <br>
+                <strong>Data: </strong> ${item.data().data} <br>
+                <strong>Status: </strong> ${item.data().status} <br>
                 </div>
 
-                <div class="d-flex gap-2 justify-content-end"
-                <button type="button" class="btn btn-danger" id="excluir">Excluir</button>
-                <button type="button" class="btn btn-info" id="alterar">Alterar</button>
+                <div class="d-flex gap-2 justify-content-end">
+                <button type="button" class="btn btn-danger" id="${item.id}">Excluir</button>
+                <button type="button" class="btn btn-info" id="${item.id}">Alterar</button>
+                </div>
             </li>
     `
-})};
+        document.querySelectorAll(".btn-danger").forEach((elemento) => {
+            elemento.addEventListener("click", (evento) => {
+              //  alert("Botão excluir acionado")
+              console.log(evento.target.id)
+              excluirTarefa(evento.target.id)
+            })
+        })
 
+        document.querySelectorAll(".btn-info").forEach((elemento) => {
+            elemento.addEventListener("click", () => {
+                if(formAtualizar.classList.contains("d-none")){
+                    formCadastrar.classList.replace("d-block", "d-none")
+                    formAtualizar.classList.replace("d-none", "d-block")
+                }
+            })
+        })
+    })
+};
 
+async function excluirTarefa(id){
+    let resultado = confirm("Tem certeza que deseja excluir?")
+    if(resultado) {
+        await deleteDoc(doc(db, "tarefa", id));
+        alert("Tarefa excluida com sucesso")
+        
+        consultarTarefa()//recarrega a lista atualizada, sem isso precisaria o user atualizar a página
+    }
+}
 
 btnTarefa.addEventListener("click", (evento) => {
     evento.preventDefault()
     console.log(nome.value, data.value, status.value)
     inserirtarefa()
+    consultarTarefa()
 })
+
+btnAtualizar.addEventListener("click", ()=>{
+
+})
+
+
+consultarTarefa()
